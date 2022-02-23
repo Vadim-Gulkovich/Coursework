1774695cbfb958622cd5a096ba31fd7a12a1c9a8
Vadim-Gulkovich <VadimGulkovich2000@gmail.com>
2022-02-23 20:56:39 +0300

    12312

--- src/main/java/com/coursework/coursework/CourseworkApplication.java
+++ src/main/java/com/coursework/coursework/CourseworkApplication.java
@@ -1,14 +1,9 @@
 package com.coursework.coursework;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan(basePackages={"com.coursework.coursework"})
+@SpringBootApplication
 public class CourseworkApplication {
 
 	public static void main(String[] args) {
